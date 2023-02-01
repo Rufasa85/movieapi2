9db2fa5ae1f6f1091e328984cb3a2f311573c806
Joe Rehfuss <jrehfuss@c02g81atq05q.domain>
2023-02-01 14:32:43 -0800

    complete

---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+USE movie_db;
+
+INSERT INTO movies(title)
+VALUES
+("Cloud Atlas"),
+("The Dark Knight"),
+("Jurassic Park"),
+("Dune"),
+("Teenage Mutant Ninja Turtles II: Secret of the Ooze");
+
+INSERT INTO reviews(movie_id,body)
+VALUES
+(1,"To speak my true true, this movie is too long"),
+(1,"Too many fake noses"),
+(2,"Most overrated of the nolan trilogy, its either 2nd or 3rd best"),
+(3,"Dinosaurs are cool"),
+(3,"Went to see it at age 7 even though my dad said it was too scary. He was right, but i loved it"),
+(4,"Sand. I hate sand"),
+(4,"Great version of this classic sci fi tale"),
+(5,"How did everyone know the rap already?"),
+(5,"Remember when everyone was obsessed with toxic waste and glowing oozes?"),
+(5,"Go ninja Go ninja Go!");
